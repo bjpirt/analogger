8bcf9cc6e292ff22ededf81781f60fddc49016ff
Ben Pirt <ben@pirt.co.uk>
2024-09-26 18:17:35 +0100

    Make a lock screen control widget

--- AnaloggerWidget/AnaloggerWidgetControl.swift
+++ AnaloggerWidget/AnaloggerWidgetControl.swift
@@ -10,68 +10,17 @@ import SwiftUI
 import WidgetKit
 
 struct AnaloggerWidgetControl: ControlWidget {
-    static let kind: String = "com.pirt.analogger.Analogger.AnaloggerWidget"
+    static let kind: String = "com.pirt.analogger.Analogger.AnaloggerControlWidget"
 
     var body: some ControlWidgetConfiguration {
-        AppIntentControlConfiguration(
-            kind: Self.kind,
-            provider: Provider()
-        ) { value in
-            ControlWidgetToggle(
-                "Start Timer",
-                isOn: value.isRunning,
-                action: StartTimerIntent(value.name)
-            ) { isRunning in
-                Label(isRunning ? "On" : "Off", systemImage: "timer")
-            }
+        StaticControlConfiguration(
+            kind: Self.kind
+        ) {
+            ControlWidgetButton(action: LogShotIntent()) {
+                Label("Log Shot", systemImage: "camera.aperture")
+              }
         }
-        .displayName("Timer")
-        .description("A an example control that runs a timer.")
-    }
-}
-
-extension AnaloggerWidgetControl {
-    struct Value {
-        var isRunning: Bool
-        var name: String
-    }
-
-    struct Provider: AppIntentControlValueProvider {
-        func previewValue(configuration: TimerConfiguration) -> Value {
-            AnaloggerWidgetControl.Value(isRunning: false, name: configuration.timerName)
-        }
-
-        func currentValue(configuration: TimerConfiguration) async throws -> Value {
-            let isRunning = true // Check if the timer is running
-            return AnaloggerWidgetControl.Value(isRunning: isRunning, name: configuration.timerName)
-        }
-    }
-}
-
-struct TimerConfiguration: ControlConfigurationIntent {
-    static let title: LocalizedStringResource = "Timer Name Configuration"
-
-    @Parameter(title: "Timer Name", default: "Timer")
-    var timerName: String
-}
-
-struct StartTimerIntent: SetValueIntent {
-    static let title: LocalizedStringResource = "Start a timer"
-
-    @Parameter(title: "Timer Name")
-    var name: String
-
-    @Parameter(title: "Timer is running")
-    var value: Bool
-
-    init() {}
-
-    init(_ name: String) {
-        self.name = name
-    }
-
-    func perform() async throws -> some IntentResult {
-        // Start the timer…
-        return .result()
+        .displayName("Log Shot")
+        .description("Log a shot in Analogger")
     }
 }
